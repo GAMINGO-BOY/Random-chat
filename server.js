6ee9b96f0a8dbe2ebc34cd85260b8a452051167a
GAMINGO-BOY <dhirajkumar56138@gmail.com>
2026-09-19 01:30:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,28 +12,108 @@ const io = new Server(server, {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let waitingUser = null;
-let onlineUsersCount = 0;
+let waitingQueue = [];
 
-function pairUsers(socket) {
-    if (waitingUser && waitingUser.id !== socket.id) {
-        const room = `room_${socket.id}_${waitingUser.id}`;
-        
-        socket.join(room);
-        waitingUser.join(room);
+// Real-time connected count broadcast karne ka function
+function broadcastOnlineCount() {
+    const activeCount = io.sockets.sockets.size;
+    io.emit('update_user_count', activeCount);
+}
 
-        socket.room = room;
-        waitingUser.room = room;
+function removeFromQueue(socketId) {
+    waitingQueue = waitingQueue.filter(id => id !== socketId);
+}
 
-        io.to(room).emit('chat_start', 'You are now connected with a random stranger!');
-        waitingUser = null;
-    } else {
-        waitingUser = socket;
-        socket.emit('waiting', 'Looking for an available stranger...');
+function matchUsers() {
+    while (waitingQueue.length >= 2) {
+        const id1 = waitingQueue.shift();
+        const id2 = waitingQueue.shift();
+
+        const socket1 = io.sockets.sockets.get(id1);
+        const socket2 = io.sockets.sockets.get(id2);
+
+        if (socket1 && socket2) {
+            const room = `room_${id1}_${id2}`;
+            socket1.join(room);
+            socket2.join(room);
+
+            socket1.room = room;
+            socket2.room = room;
+
+            socket1.partnerId = id2;
+            socket2.partnerId = id1;
+
+            io.to(room).emit('chat_start', 'You are now connected with a stranger!');
+        } else if (socket1) {
+            waitingQueue.unshift(id1);
+        } else if (socket2) {
+            waitingQueue.unshift(id2);
+        }
     }
 }
 
 function leaveRoom(socket) {
+    removeFromQueue(socket.id);
+
+    if (socket.room) {
+        socket.to(socket.room).emit('stranger_left', 'Stranger has left the chat.');
+        
+        const roomSockets = io.sockets.adapter.rooms.get(socket.room);
+        if (roomSockets) {
+            for (const socketId of roomSockets) {
+                const clientSocket = io.sockets.sockets.get(socketId);
+                if (clientSocket) {
+                    clientSocket.leave(socket.room);
+                    clientSocket.room = null;
+                    clientSocket.partnerId = null;
+                }
+            }
+        }
+        socket.room = null;
+        socket.partnerId = null;
+    }
+}
+
+io.on('connection', (socket) => {
+    // Connection par instant updated count sabhi clients ko bhejna
+    broadcastOnlineCount();
+
+    waitingQueue.push(socket.id);
+    socket.emit('waiting', 'Looking for a stranger...');
+    matchUsers();
+
+    socket.on('send_message', (msg) => {
+        if (socket.room) {
+            socket.to(socket.room).emit('receive_message', msg);
+        }
+    });
+
+    socket.on('typing', (isTyping) => {
+        if (socket.room) {
+            socket.to(socket.room).emit('display_typing', isTyping);
+        }
+    });
+
+    socket.on('next_user', () => {
+        leaveRoom(socket);
+        if (!waitingQueue.includes(socket.id)) {
+            waitingQueue.push(socket.id);
+        }
+        socket.emit('waiting', 'Looking for a new stranger...');
+        matchUsers();
+    });
+
+    socket.on('disconnect', () => {
+        leaveRoom(socket);
+        // Disconnect par updated count broadcast karna
+        broadcastOnlineCount();
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
     if (waitingUser && waitingUser.id === socket.id) {
         waitingUser = null;
     }
