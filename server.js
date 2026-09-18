6084cd592deaec39cf17826e515ec6a22c73f870
GAMINGO-BOY <dhirajkumar56138@gmail.com>
2026-09-19 01:21:30 +0530

    Refactor Socket.io user pairing and room management

--- server.js
+++ server.js
@@ -6,23 +6,16 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
-// Socket.io setup with CORS enabled
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+    cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
-// Static files serve karne ke liye 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 let waitingUser = null;
+let onlineUsersCount = 0;
 
-io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.id}`);
-
-    // Users ko aapas mein pair/match karna
+function pairUsers(socket) {
     if (waitingUser && waitingUser.id !== socket.id) {
         const room = `room_${socket.id}_${waitingUser.id}`;
         
@@ -32,8 +25,70 @@ io.on('connection', (socket) => {
         socket.room = room;
         waitingUser.room = room;
 
-        io.to(room).emit('chat_start', 'Aap ek stranger se connect ho gaye hain!');
+        io.to(room).emit('chat_start', 'You are now connected with a random stranger!');
+        waitingUser = null;
+    } else {
+        waitingUser = socket;
+        socket.emit('waiting', 'Looking for an available stranger...');
+    }
+}
+
+function leaveRoom(socket) {
+    if (waitingUser && waitingUser.id === socket.id) {
+        waitingUser = null;
+    }
+    
+    if (socket.room) {
+        socket.to(socket.room).emit('stranger_left', 'Stranger has disconnected.');
         
+        const roomSockets = io.sockets.adapter.rooms.get(socket.room);
+        if (roomSockets) {
+            for (const socketId of roomSockets) {
+                const clientSocket = io.sockets.sockets.get(socketId);
+                if (clientSocket) {
+                    clientSocket.leave(socket.room);
+                    clientSocket.room = null;
+                }
+            }
+        }
+        socket.room = null;
+    }
+}
+
+io.on('connection', (socket) => {
+    onlineUsersCount++;
+    io.emit('update_user_count', onlineUsersCount);
+
+    pairUsers(socket);
+
+    socket.on('send_message', (msg) => {
+        if (socket.room) {
+            socket.to(socket.room).emit('receive_message', msg);
+        }
+    });
+
+    socket.on('typing', (isTyping) => {
+        if (socket.room) {
+            socket.to(socket.room).emit('display_typing', isTyping);
+        }
+    });
+
+    socket.on('next_user', () => {
+        leaveRoom(socket);
+        pairUsers(socket);
+    });
+
+    socket.on('disconnect', () => {
+        onlineUsersCount = Math.max(0, onlineUsersCount - 1);
+        io.emit('update_user_count', onlineUsersCount);
+        leaveRoom(socket);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
         waitingUser = null;
     } else {
         waitingUser = socket;
